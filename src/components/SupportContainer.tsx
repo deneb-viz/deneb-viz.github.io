9392cf77179ae0b9ea486b9cb96642d05075d207
giammaria <giammariam@gmail.com>
2024-01-15 13:53:39 -0500

    provided additional comments in SupportContainer.tsx

--- src/components/SupportContainer.tsx
+++ src/components/SupportContainer.tsx
@@ -1,16 +1,25 @@
 // /src/components/SupportContainer.tsx
+
+// React modules
 import React, { useEffect, useState } from "react";
+// array of expert data
 import expertsData from "../../static/data/community-experts.json";
 
+// Component to render community experts and their metadata
 const SupportContainer = () => {
+  // State management
   const [experts, setExperts] = useState([]);
 
+  // useEffect hook to perform shuffling and data fetching after the component renders
+  // state update will trigger a re-render
+  // empty dependency array to ensure that this effect only runs once
   useEffect(() => {
-    // Shuffle the array randomly
+    // Shuffle the array of IExperts randomly
     const shuffledExperts = shuffleArray(expertsData as IExpert[]);
     setExperts(shuffledExperts);
   }, []);
 
+  // return JSX for the SupportContainer's UI
   return (
     <div className="support-container">
       {experts.map((expert) => (
@@ -51,6 +60,7 @@ const SupportContainer = () => {
 
 export default SupportContainer;
 
+// Community expert interface to be used with expertsData array
 interface IExpert {
   name: string;
   imageSrc: string;
@@ -59,20 +69,27 @@ interface IExpert {
   contributionAreas: IContributionArea[];
 }
 
+// Community expert contribution area interface to be used within experts
 interface IContributionArea {
   name: "Deneb" | "Vega" | "Vega-Lite";
   abbreviation: "D" | "V" | "VL";
 }
 
+// Function to shuffle the array using Fisher-Yates algorithm
+// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
 const shuffleArray = (array: IExpert[]) => {
-  // Fisher-Yates shuffle algorithm
+  // variables to track the current index and current random index
   let currentIndex = array.length,
     randomIndex: number;
 
+  // while there are remaining elements
   while (currentIndex !== 0) {
+    //assign randomIndex a random number that is >= 0 && < currentIndex
     randomIndex = Math.floor(Math.random() * currentIndex);
+
     currentIndex--;
 
+    //swap positions between currentIndex and randomIndex
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
       array[currentIndex],
