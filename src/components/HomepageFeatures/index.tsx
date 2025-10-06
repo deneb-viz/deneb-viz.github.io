253a0364a17b58fed12cc11801f3834946e94246
Daniel Marsh-Patrick <daniel@coacervo.co>
2025-10-06 15:00:43 +1300

    styles: improve button responsiveness on smaller screens

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -61,7 +61,7 @@ const FeatureList: FeatureItem[] = [
 
 function Feature({ title, Svg, description }: FeatureItem) {
   return (
-    <div className={clsx("col col--3")}>
+    <div className={clsx("col col--12 col--md-6 col--lg-3")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
